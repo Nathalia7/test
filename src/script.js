8003d215139d3c2f2308000444a79e9b814d766a
Naty <toffee1903@gmail.com>
2019-08-14 18:39:40 +0300

    Test1

--- src/script.js
+++ src/script.js
@@ -14,13 +14,40 @@ $(document).ready(function () {
         variableWidth: true,
     });
 
-    $('.partners-slider').slick({
+    $('.partners-slider1').slick({
+        mobileFirst: false,
+        prevArrow: ".partners-slider-prev1",
+        nextArrow: ".partners-slider-next1",
+        infinite: true,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $('.partners-slider2').slick({
         mobileFirst: false,
         prevArrow: ".partners-slider-prev",
         nextArrow: ".partners-slider-next",
         infinite: true,
-        slidesToShow: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
+
     $('a').click(function () {
         if(this.href.includes("#")){
             $('html, body').animate({
